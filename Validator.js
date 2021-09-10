d82f7625a4226938f89434ff85d94ebfe67380f0
longho592002 <longnguyen19052018@gmail.com>
2021-09-10 16:17:10 +0700

    upload

---
+++ Validator.js
@@ -0,0 +1,14 @@
+
+// Đối tượng `Validator`
+function Validator(options) {
+    console.log()
+}
+
+// Định nghĩa Rules
+Validator.isRequired = function () {
+
+}
+
+Validator.isEmail = function () {
+    
+}
