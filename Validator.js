dacc290cb26fc940ad8c79dc7d65c095d6324e3d
longho592002 <longnguyen19052018@gmail.com>
2021-09-18 09:49:35 +0700

    upload

--- Validator.js
+++ Validator.js
@@ -1,12 +1,20 @@
 
 // Đối tượng `Validator`
 function Validator(options) {
+    function getParent(element, selector) {
+        while(element.parentElement) {
+            if(element.parentElement.matches(selector))
+                return element.parentElement
+            element = element.parentElement;
+        }   
 
+    }
     var selectorRules = {};
     // Hàm thực hiện validate
     function validate(inputElement,rule) {
-        var errorMessage = rule.test(inputElement.value);
-        var errorElement = inputElement.parentElement.querySelector(options.errorMessage);
+        // var errorElement = getParent(inputElement, '.form-group')
+        var errorMessage;
+        var errorElement = getParent(inputElement,options.formGroupSelector).querySelector(options.errorMessage);
         
         // Lấy ra các rules của selector
         var rules = selectorRules[rule.selector]    
@@ -14,15 +22,26 @@ function Validator(options) {
         // Lặp qua từng rule và kiểm tra
         // Nếu có lỗi thì dừng việc kiểm tra
         for(var i = 0; i < rules.length; i++) {
-            errorMessage = rules[i](inputElement.value);
+            switch(inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector = ':checked')
+                    );
+                    break;
+                default :
+                    errorMessage = rules[i](inputElement.value);
+            }
+
+            
             if(errorMessage) break;
         }
                     if(errorMessage) {
                         errorElement.innerText = errorMessage;
-                        inputElement.parentElement.classList.add('invalid')
+                        getParent(inputElement,options.formGroupSelector).classList.add('invalid')
                     } else {
                         errorElement.innerText = '';
-                        inputElement.parentElement.classList.remove('invalid')
+                        getParent(inputElement,options.formGroupSelector).classList.remove('invalid')
                     }
         return !errorMessage;
     }
@@ -47,7 +66,28 @@ function Validator(options) {
                 if (typeof options.onSubmit === 'function') {
                     var enableInputs = formElement.querySelectorAll('[name]');
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        return (values[input.name] = input.value)  && values;
+                        
+                        switch(input.type) {
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked')
+                                break;
+                            case 'checkbox':
+                                if(!input.matches(':checked'))  {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(value[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.file
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+                        return values;
                     }, {});
                     options.onSubmit(formValues)
                 } 
@@ -69,11 +109,11 @@ function Validator(options) {
                 selectorRules[rule.selector] = [rule.test];
             }
             
-            var inputElement = formElement.querySelector(rule.selector);
+            var inputElements = formElement.querySelectorAll(rule.selector);
             
-            if (inputElement) {
-                // Xử lý trường hợp blur khỏi input
-                inputElement.onblur = function () {
+            Array.from(inputElements).forEach(function (inputElement) {
+                 // Xử lý trường hợp blur khỏi input
+                 inputElement.onblur = function () {
                     // value: input Element.value
                     // test func: rule.test
                     validate(inputElement,rule)
@@ -81,11 +121,12 @@ function Validator(options) {
                 
                 // Xử lý  mỗi khi người dùng nhập input
                 inputElement.oninput = function () {
-                    var errorElement = inputElement.parentElement.querySelector('.form-message');
+                    var errorElement = getParent(inputElement,options.formGroupSelector).querySelector('.form-message');
                     errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid')
+                    getParent(inputElement,options.formGroupSelector).classList.remove('invalid')
                 }
-            }
+              })
+            
         })
     }
 }
@@ -98,7 +139,7 @@ Validator.isRequired = function (selector, message) {
     return {
         selector:selector,
         test: function (value) {
-            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
+            return value ? undefined : message || 'Vui lòng nhập trường này'
         }
     }
     
